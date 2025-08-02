129faa001dff3153e808e6330fcbbe217495aa26
MamitianaAntonio <ratolojanaharyantonio1@gmail.com>
2025-08-02 16:39:32 +0000

    add LibraryDAO class and their feature and Test manual in Main class

---
+++ src/main/java/antonio/geek/Entities/LibraryDAO.java
@@ -0,0 +1,44 @@
+package antonio.geek.Entities;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class LibraryDAO {
+    List<Book> books;
+
+    public LibraryDAO() {
+        this.books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        this.books.add(book);
+    }
+
+    public List<BorrowableBook> getBorrowableBookNotBorrow () {
+        return books.stream()
+                .filter(book -> book instanceof BorrowableBook)
+                .map(book -> (BorrowableBook) book)
+                .filter(book -> !book.isBorrowed())
+                .collect(Collectors.toList());
+    }
+
+    public double getTotalEbooksSize () {
+        return books.stream()
+                .filter(book -> book instanceof Ebook)
+                .map(book -> (Ebook) book)
+                .mapToDouble(Ebook::getSizeInMb)
+                .sum();
+    }
+
+    public List<String> getExpensiveBooksTitle () {
+        return books.stream()
+                .filter(book -> book.getPrice() > 20)
+                .map(Book::getTitle)
+                .collect(Collectors.toList());
+    }
+
+    public double getSumOfAllPriceBooks () {
+        return books.stream().mapToDouble(Book::getPrice).sum();
+    }
+}
--- src/main/java/antonio/geek/Main.java
+++ src/main/java/antonio/geek/Main.java
@@ -1,17 +1,31 @@
 package antonio.geek;
+import antonio.geek.Entities.Book;
+import antonio.geek.Entities.BorrowableBook;
+import antonio.geek.Entities.Ebook;
+import antonio.geek.Entities.LibraryDAO;
+import java.util.ArrayList;
+import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        LibraryDAO libraryDAO = new LibraryDAO();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        libraryDAO.addBook(new BorrowableBook("Les Misérables", "Victor Hugo", 15.0, 15, true));
+        libraryDAO.addBook(new BorrowableBook("Harry Potter", "J.K. Rowling", 22.0, 10, false));
+        libraryDAO.addBook(new BorrowableBook("L’Étranger", "Albert Camus", 18.5, 8, false));
+        libraryDAO.addBook(new BorrowableBook("1984", "George Orwell", 19.9, 11, true));
+        libraryDAO.addBook(new BorrowableBook("Le Comte de Monte-Cristo", "Alexandre Dumas", 25.0, 18, false));
+        libraryDAO.addBook(new Ebook("Introduction à Java", "Author1", 9.5, 120, 3));
+        libraryDAO.addBook(new Ebook("Spring Boot in Action", "Author2", 24.0, 210, 8));
+        libraryDAO.addBook(new Ebook("Clean Code", "Robert C. Martin", 28.0, 300, 5));
+
+        System.out.println("Borrowable book and not borrow :");
+        libraryDAO.getBorrowableBookNotBorrow().forEach(System.out::println);
+        System.out.println("\n Total of ebook size :");
+        System.out.println(libraryDAO.getTotalEbooksSize() + " MB");
+        System.out.println("\n Get title book superior 20");
+        libraryDAO.getExpensiveBooksTitle().forEach(System.out::println);
+        System.out.println("\n Sum of all prices of books :");
+        System.out.println(libraryDAO.getSumOfAllPriceBooks());
     }
 }
